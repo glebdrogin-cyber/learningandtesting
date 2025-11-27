8b253cc954c5f88b944446dca348b2a1edf7a353
Gleb Drogin <glebdrogin@gmail.com>
2025-11-27 12:44:26 +0100

    unit test update

--- unit_test.py
+++ unit_test.py
@@ -4,11 +4,10 @@ def add(a, b):
     return a + b
 
 class TestAddFunction(unittest.TestCase):
-    def test_add_integers(self):
-        self.assertEqual(add(2, 3), 5)
-
-    def test_add_strings(self):
-        self.assertEqual(add('hello', ' world'), 'hello world')
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
 
 if __name__ == '__main__':
     unittest.main()
